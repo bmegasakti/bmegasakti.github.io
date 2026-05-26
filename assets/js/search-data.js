bdd227f973421287f8feba6e609f214c3910f2d3
bmegasakti <brahmastra.megasakti@outlook.com>
2026-05-26 15:54:00 +0000

    Deploying to gh-pages from @ bmegasakti/bmegasakti.github.io@6e10f241c1ae4f0a44ea6c43ad3e32c47a7f9e31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "For the most up-to-date publication record, please visit Google Scholar.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
